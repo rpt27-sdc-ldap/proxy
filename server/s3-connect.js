6a252af98b0601f0515139601a137a8b7b395a6a
Mackenzie <mackenziekynoch@gmail.com>
2021-05-20 22:18:25 -0400

    Refactored s3 access to use shared config file

--- server/s3-connect.js
+++ server/s3-connect.js
@@ -2,8 +2,13 @@ const S3 = require("@aws-sdk/client-s3");
 
 const REGION = 'us-west-2';
 
+const {fromIni} = require("@aws-sdk/credential-provider-ini");
 const s3Client = new S3.S3Client({
-  region: REGION
+  credentials: fromIni({profile: 'admin-account'})
 });
+
+// const s3Client = new S3.S3Client({
+//   region: REGION
+// });
 
 module.exports = s3Client;
