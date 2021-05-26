64b81573ccbd315b414da5f412639db152a998e8
Mackenzie <mackenziekynoch@gmail.com>
2021-05-26 09:04:47 -0400

    config for deployment

---
+++ ec2-connect.sh
@@ -0,0 +1,3 @@
+chmod 400 rpt27-fec-audible-proxy.pem
+
+ssh -i "/Users/mackenzie/Documents/HackReactor/FEC/mkk-proxy/rpt27-fec-audible-proxy.pem" ec2-user@ec2-34-219-131-242.us-west-2.compute.amazonaws.com
